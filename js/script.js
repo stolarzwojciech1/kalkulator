82d91396dacb9ac3f8b36e764f03bdf99c52a7b9
Wojciech Stolarz <stolarzwojciech1@gmail.com>
2023-02-08 17:45:56 +0100

    zmiany

--- js/script.js
+++ js/script.js
@@ -1,17 +1,69 @@
-console.log("Cześć");
-let usdElement = document.querySelector(".js-usd");
-let euroElement = document.querySelector(".js-euro");
-let formElement = document.querySelector(".form");
-let valueElement = document.querySelector(".js-value");
+{
 
-formElement.addEventListener("submit", (event)=>{
+
+const calkulate = (usdElement, euroElement) =>{
+ 
+    const usd=usdElement.value;
+const euro=euroElement.value;
+
+const value=usd*euro;
+return value;
+}
+const newresult = (usdElement, euroElement) =>{
+const valueElement = document.querySelector(".js-value");
+    valueElement.innerText= calkulate(usdElement, euroElement).toFixed(2);
+}
+
+const sumbit = (event) =>{
+
 event.preventDefault();
 
-let usd=usdElement.value;
-let euro=euroElement.value;
 
-let value=usd*euro;
+const usdElement = document.querySelector(".js-usd");
+const euroElement = document.querySelector(".js-euro");
 
-valueElement.innerText= value.toFixed(2);
-})
+newresult(usdElement, euroElement);
 
+}
+
+const init = () =>{
+
+const formElement = document.querySelector(".form");
+
+
+formElement.addEventListener("submit", sumbit);
+
+
+
+}
+init();
+}
+/*odtąd
+const welcome = () =>{
+    console.log("Cześć");
+    }
+    
+    welcome();
+    
+    const button1 = () => {
+    const themeName = document.querySelector(".themeName");
+    const body = document.querySelector(".body");
+    
+    body.classList.toggle("body__dark");
+    themeName.innerText = body.classList.contains("body__dark") ? "Tło 1" : "Tło 2";
+    
+    
+    }
+    
+    const init = () => {
+        
+    const button = document.querySelector(".section__button");
+    
+    button.addEventListener("click", button1);
+    
+    
+    
+    }
+    init();
+
+*/
