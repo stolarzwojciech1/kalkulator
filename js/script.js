d10cd90eaa37b5b778051e7e496ffe6d823bb27b
Wojciech Stolarz <stolarzwojciech1@gmail.com>
2023-02-09 12:24:08 +0100

    zmiany

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 {
 
 
-const calkulate = (usdElement, euroElement) =>{
+const calculate = (usdElement, euroElement) =>{
  
     const usd=usdElement.value;
 const euro=euroElement.value;
 
-const value=usd*euro;
-return value;
+return usd*euro;
 }
 const newresult = (usdElement, euroElement) =>{
 const valueElement = document.querySelector(".js-value");
-    valueElement.innerText= calkulate(usdElement, euroElement).toFixed(2);
+    valueElement.innerText= calculate(usdElement, euroElement).toFixed(2);
 }
 
 const sumbit = (event) =>{
@@ -38,32 +37,3 @@ formElement.addEventListener("submit", sumbit);
 }
 init();
 }
-/*odtąd
-const welcome = () =>{
-    console.log("Cześć");
-    }
-    
-    welcome();
-    
-    const button1 = () => {
-    const themeName = document.querySelector(".themeName");
-    const body = document.querySelector(".body");
-    
-    body.classList.toggle("body__dark");
-    themeName.innerText = body.classList.contains("body__dark") ? "Tło 1" : "Tło 2";
-    
-    
-    }
-    
-    const init = () => {
-        
-    const button = document.querySelector(".section__button");
-    
-    button.addEventListener("click", button1);
-    
-    
-    
-    }
-    init();
-
-*/
